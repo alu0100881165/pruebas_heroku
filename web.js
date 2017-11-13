04a598939d6470b836d9516cc36ede0ae7f22c54
fridar <ccpdanielnuezwehbe@gmail.com>
2017-11-13 18:25:52 +0000

    Arreglar heroku open

--- web.js
+++ web.js
@@ -1,23 +1,18 @@
-//importar
 var express = require('express');
-//instanciar
 var app = express();
-var port1 = process.env.PORT || 8080;
 
-//Archivos estaticos css, js, img, html
-app.use(express.static('.'));
+app.set('port', (process.env.PORT || 5000));
 
-//ruteo
-app.get('/', function(req, res){
-res.sendfile(__dirname + '/index2.html');
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
 
-//escuchar
-var server = app.listen(port1, function(){
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
-
-})
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
