95be35308a44cb6f02371bf0fc5b739e660bdfb1
fridar <ccpdanielnuezwehbe@gmail.com>
2017-11-13 18:03:34 +0000

    Your Message

--- web.js
+++ web.js
@@ -12,7 +12,7 @@ res.sendfile(__dirname + '/index2.html');
 });
 
 //escuchar
-var server = app.listen(8080, function () {
+var server = app.listen(9090, function () {
 
   var host = server.address().address
   var port = server.address().port
