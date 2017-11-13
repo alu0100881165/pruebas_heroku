bdc118480f640833e3925740a281f9052b5c2867
fridar <ccpdanielnuezwehbe@gmail.com>
2017-11-13 18:32:02 +0000

    Arreglar heroku open

--- web.js
+++ web.js
@@ -3,14 +3,10 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.use(express.static('/public'));
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.sendfile( __dirname + '/public/index2.html');
 });
 
 app.listen(app.get('port'), function() {
